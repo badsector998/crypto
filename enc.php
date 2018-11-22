07445d8c6085c7d09d7f1bb2498b4c134d6e3f64
DadangRokes <31819806+badsector998@users.noreply.github.com>
2018-11-22 21:55:01 +0700

    Update enc.php
    nambah dokumentasi

--- enc.php
+++ enc.php
@@ -2,25 +2,25 @@
 	$key = $_POST['key'];
 	$plaintext = $_POST['plaintext'];
 
-	$keyarr = unpack("C*", $key);
-	$plainarr = unpack("C*", $plaintext);
-	$len = count($plainarr);
-	$len2 = count($keyarr);
+	$keyarr = unpack("C*", $key); //convert string jadi ascii
+	$plainarr = unpack("C*", $plaintext); //convert string jadi ascii
+	$len = count($plainarr); //hitung panjang array
+	$len2 = count($keyarr); //hitung panjang array
 
-	for($i=$len2;$i < $len-$len2;$i++){
+	for($i=$len2;$i < $len-$len2;$i++){ //proses padding key
 		for($j=1;$j < $len2;$j++){
 			array_push($keyarr, $keyarr[$j]);
 		}
 	}
 
-	var_dump($plainarr);
+	var_dump($plainarr); //debugging mode, cek jenis variabel dan outputnya
 	var_dump($keyarr);
 
-	for($k = 1;$k<$len;$k++){
+	for($k = 1;$k<$len;$k++){ //proses xor
 		$keyarr[$k] ^= $plainarr[$k];
 	}
 
-	var_dump($keyarr);
-	$key = call_user_func_array('pack', array_merge(array('C*'),$keyarr));
+	var_dump($keyarr); //debugging mode, cek jenis variabel dan outputnya
+	$key = call_user_func_array('pack', array_merge(array('C*'),$keyarr)); //convert ASCII ke string
 	var_dump($key);
 ?>
